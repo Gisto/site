5a0e4b89e99c4d61adcf4a7ad17a4c4a0867ac4f
sasha <sanusart@gmail.com>
2025-03-08 15:37:22 +0200

    fix layout

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -60,6 +60,18 @@ export const Footer = () => {
         >
           <img className="size-4" src="https://cdn.simpleicons.org/github/3f83a8" alt="GitHub" />
         </Button>
+        <Separator orientation="vertical" className="hidden sm:block" />
+        <a
+          target="_blank"
+          className="underline hover:underline-offset-4 hidden sm:block"
+          href="https://github.com/sponsors/Gisto"
+        >
+          <div className="flex items-center gap-2">
+            <Heart className="text-danger size-4" /> Sponsor Gisto
+          </div>
+        </a>
+      </div>
+      <div className="mt-4 visible sm:hidden">
         <Separator orientation="vertical" />
         <a
           target="_blank"
